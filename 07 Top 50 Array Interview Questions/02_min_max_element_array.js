c430c3bff440e1376d85ce7a6730d4a57e2f5396
MohitGupta <krishgupta1630@gmail.com>
2023-05-11 23:39:19 +0530

    min max element problem

---
+++ 07 Top 50 Array Interview Questions/02_min_max_element_array.js
@@ -0,0 +1,31 @@
+/*
+👉 Write a program to find minimum and maximum element in array.
+
+Input  : arr[] = {3, 4, 1, -5, 10}
+Output: Min = -5, Max = 10
+*/
+
+
+function findMinMax(arr){
+    // let assume first element as min or max
+    let min = arr[0];
+    let max = arr[1];
+
+    for(let i=0; i<arr.length; i++){
+        if(arr[i] < min){
+            min = arr[i];
+        }else if(arr[i] > max){
+            max = arr[i];
+        }
+    }
+    
+    return [min, max];
+}
+
+const [min, max] = findMinMax([3, 4, 1, -5, 10]);
+console.log(`Minimum element: ${min}\n Maximum element: ${max}`);
+
+/* 
+Time complexity : O(n)
+Space Complexity: O(1)
+*/
